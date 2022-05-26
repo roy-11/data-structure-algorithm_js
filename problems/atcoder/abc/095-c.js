8a90945527a72a2dd8d125c82cf7498740ed14e3
roy-11 <63440731+roy-11@users.noreply.github.com>
2022-05-26 15:22:41 +0900

    modify 095-c

--- problems/atcoder/abc/095-c.js
+++ problems/atcoder/abc/095-c.js
@@ -1,3 +1,4 @@
+// AC1-1
 function main(input) {
   let [acost, bcost, abcost, targetX, targetY] = input
     .split(" ")
@@ -6,10 +7,10 @@ function main(input) {
   let minCost = undefined;
   const times = Math.max(targetX, targetY) * 2;
   for (let ab = 0; ab <= times; ab += 2) {
-    const x = Math.max(0, targetX - ab);
-    const y = Math.max(0, targetY - ab);
+    const x = Math.max(0, targetX - Math.floor(ab / 2));
+    const y = Math.max(0, targetY - Math.floor(ab / 2));
 
-    let total = ab * abcost * 2;
+    let total = ab * abcost;
     total += x * acost;
     total += y * bcost;
 
@@ -23,3 +24,69 @@ function main(input) {
 main("1500 2000 1600 3 2");
 main("1500 2000 1900 3 2");
 main("1500 2000 500 90000 100000");
+
+// AC1-2
+function main2(input) {
+  let [acost, bcost, abcost, targetX, targetY] = input
+    .split(" ")
+    .map((e) => parseInt(e));
+
+  let minCost = undefined;
+  const times = 10 ** 5 * 2; // 上限で全探索
+  for (let ab = 0; ab <= times; ab += 2) {
+    const x = Math.max(0, targetX - Math.floor(ab / 2));
+    const y = Math.max(0, targetY - Math.floor(ab / 2));
+
+    let total = ab * abcost;
+    total += x * acost;
+    total += y * bcost;
+
+    if (minCost === undefined) minCost = total;
+    minCost = Math.min(minCost, total);
+  }
+  console.log(minCost);
+}
+
+// AC2
+function main3(input) {
+  let [acost, bcost, abcost, targetX, targetY] = input
+    .split(" ")
+    .map((e) => parseInt(e));
+
+  let minCost = undefined;
+  const times = Math.max(targetX, targetY) * 2;
+  for (let ab = 0; ab <= times; ab++) {
+    const x = Math.max(0, targetX - ab);
+    const y = Math.max(0, targetY - ab);
+
+    let total = ab * abcost * 2; // コスト２倍
+    total += x * acost;
+    total += y * bcost;
+
+    if (minCost === undefined) minCost = total;
+    minCost = Math.min(minCost, total);
+  }
+  console.log(minCost);
+}
+
+// AC3
+function main4(input) {
+  let [acost, bcost, abcost, targetX, targetY] = input
+    .split(" ")
+    .map((e) => parseInt(e));
+
+  let minCost = undefined;
+  const times = 10 ** 5; // 上限で全探索
+  for (let ab = 0; ab <= times; ab++) {
+    const x = Math.max(0, targetX - ab);
+    const y = Math.max(0, targetY - ab);
+
+    let total = ab * abcost * 2;
+    total += x * acost;
+    total += y * bcost;
+
+    if (minCost === undefined) minCost = total;
+    minCost = Math.min(minCost, total);
+  }
+  console.log(minCost);
+}
